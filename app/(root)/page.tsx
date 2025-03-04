6d8653485fc564ac4e7191f1fb3e0298c77dc0cc
dykciak <dawid.dykto@gmail.com>
2025-03-05 00:07:40 +0100

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import SearchForm from "@//components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 export default async function Home({
 	searchParams,
 }: {
 	searchParams: Promise<{ query?: string }>;
 }) {
 	const query = (await searchParams).query;
+
+	const posts = [
+		{
+			_createAt: new Date(),
+			views: 55,
+			author: { _id: 1, name: "John" },
+			_id: 1,
+			description: "This is a description",
+			image:
+				"https://cdn.pixabay.com/photo/2023/02/12/19/26/robot-7785785_1280.jpg",
+			category: "Robots",
+			title: "We Robots",
+		},
+	];
+
 	return (
 		<>
 			<section className="pink_container">
@@ -17,6 +33,20 @@ export default async function Home({
 					Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
 				</p>
 				<SearchForm query={query} />
+			</section>
+			<section className="section_container">
+				<p className="text-30-semibold">
+					{query ? `Search results for "${query}"` : "All Startups"}
+				</p>
+				<ul className="mt-7 card_grid">
+					{posts?.length > 0 ? (
+						posts.map((post: StartupCardType, index: number) => (
+							<StartupCard key={post?._id} post={post} />
+						))
+					) : (
+						<p className="no-results">No startups found</p>
+					)}
+				</ul>
 			</section>
 		</>
 	);
